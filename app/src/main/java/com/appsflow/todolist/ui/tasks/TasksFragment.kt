6e6a79ae6e6e639a2b46e431c1cd294c2b849aa1
Dmytro Hurskyi <dima4365@gmail.com>
2021-07-11 13:03:16 +0300

    added tasks fragment top menu

--- app/src/main/java/com/appsflow/todolist/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/appsflow/todolist/ui/tasks/TasksFragment.kt
@@ -1,12 +1,16 @@
 package com.appsflow.todolist.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.appsflow.todolist.R
 import com.appsflow.todolist.databinding.FragmentTasksBinding
+import com.appsflow.todolist.utils.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,5 +35,17 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner){
             tasksAdapter.submitList(it)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_top_fragment_task, menu)
+
+        val searchItem = menu.findItem(R.id.menu_action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged{
+            //TODO:update search query
+        }
+
     }
 }
---
+++ app/src/main/java/com/appsflow/todolist/utils/ViewExtansions.kt
@@ -0,0 +1,16 @@
+package com.appsflow.todolist.utils
+
+import android.widget.SearchView
+
+inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            listener(newText.orEmpty())
+            return true
+        }
+    })
+}
